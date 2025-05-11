e392c330738036512812a35728ec14b543c6f8d5
Edurne Pascual <herenvardo@gmail.com>
2025-05-11 19:46:26 +0200

    test: add validation for non-overlapping keys between ClassFeature and InnateTrait

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -8,15 +8,23 @@ describe('Global enums', () => {
     it('Reputation should be a string-based enum', () => {
         expectStringEnum(Reputation);
     });
+    it('ClassFeature and InnateTrait must not have overlapping keys', () => {
+        const classFeatureKeys = Object.keys(ClassFeature);
+        const innateTraitKeys = Object.keys(InnateTrait);
+        classFeatureKeys.forEach((key) => {
+            expect(innateTraitKeys).not.toContain(key);
+        });
+        innateTraitKeys.forEach((key) => {
+            expect(classFeatureKeys).not.toContain(key);
+        });
+    });
     it('Feature should be a string-based enum', () => {
         expectStringEnum(Feature);
     });
     it('The number of features should be equal to the sum of ClassFeature and InnateTrait', () => {
-        // This also implies no key conflicts between ClassFeature and InnateTrait
         const featureKeys = Object.keys(Feature);
         const classFeatureKeys = Object.keys(ClassFeature);
         const innateTraitKeys = Object.keys(InnateTrait);
         expect(featureKeys.length).toBe(classFeatureKeys.length + innateTraitKeys.length);
-        expect(featureKeys).toEqual([...classFeatureKeys, ...innateTraitKeys]);
     });
 });
