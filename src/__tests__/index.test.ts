b452f53e8d0ce9c8296c8132ed0c58152f1c8d4e
Edurne Pascual <herenvardo@gmail.com>
2025-05-11 19:44:16 +0200

    test: add validation for Feature count against ClassFeature and InnateTrait

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { Languages, Reputation, Feature } from '../index';
+import { Languages, Reputation, Feature, ClassFeature, InnateTrait } from '../index';
 import { expectStringEnum } from '../testingHelpers';
 
 describe('Global enums', () => {
@@ -10,5 +10,13 @@ describe('Global enums', () => {
     });
     it('Feature should be a string-based enum', () => {
         expectStringEnum(Feature);
+    });
+    it('The number of features should be equal to the sum of ClassFeature and InnateTrait', () => {
+        // This also implies no key conflicts between ClassFeature and InnateTrait
+        const featureKeys = Object.keys(Feature);
+        const classFeatureKeys = Object.keys(ClassFeature);
+        const innateTraitKeys = Object.keys(InnateTrait);
+        expect(featureKeys.length).toBe(classFeatureKeys.length + innateTraitKeys.length);
+        expect(featureKeys).toEqual([...classFeatureKeys, ...innateTraitKeys]);
     });
 });
